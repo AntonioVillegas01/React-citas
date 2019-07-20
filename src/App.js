ce27be9cfec48b8e3477f79ad115c4a7687b50e6
MARCO ANTONIO GARCIA <antonio_villegas@hotmail.com>
2019-07-20 01:18:16 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import Header from "./componentes/Header";
+import AgregarCita from "./componentes/AgregarCita";
+import ListaCitas from "./componentes/ListaCitas";
+
+class App extends Component {
+
+    state = {
+        citas: []
+    }
+
+
+    componentDidMount() {
+        //Leemos los datos
+        const citasLS = localStorage.getItem('citas')
+        if(citasLS){
+            this.setState({
+                citas: JSON.parse(citasLS)
+            })
+        }
+
+    }
+
+    componentDidUpdate() {
+        //Agregamos los datos al local Storage
+        //solo almacena cadenas de texto por eso usamos stringify
+        localStorage.setItem('citas', JSON.stringify(this.state.citas))
+    }
+
+    crearCita = (nuevaCita) => {
+
+        const citas = [...this.state.citas, nuevaCita];
+      //  console.log(citas)
+
+
+        this.setState({
+            citas : citas
+        })
+    }
+
+    borrarCita = (id) => {
+      //  console.log(id)
+
+        //obtener copia del state
+        const citasActuales = [...this.state.citas]
+
+        //borrar el elemento del state
+        const citas = citasActuales.filter(cita => cita.id !== id)
+
+        //actualizar el state
+        this.setState({
+            citas
+        })
+    }
+
+  render() {
+    return (
+        <div className='container'>
+            <Header
+                titulo = {'Administrador de pacientes de veterinaria'}
+            />
+            <div className="row">
+                <div className="col-md-6">
+                    <AgregarCita
+                        crearCita={this.crearCita}
+                    />
+                </div>
+                <div className="col-md-6">
+                    <ListaCitas
+                        citas={this.state.citas}
+                        borrarCita={this.borrarCita}
+                    />
+                </div>
+            </div>
+
+        </div>
+    );
+  }
+}
+
+export default App;
